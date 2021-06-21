f23c260309fd71c45c2e03d6adc6222b8a8b5dbf
RSimer <65471088+RSimer@users.noreply.github.com>
2021-06-21 11:20:14 -0700

    added the javascript I have, sorry i dont have a lot

--- script.js
+++ script.js
@@ -10,3 +10,48 @@
 // recieve back playlist data from data api
 // playlist data into understandable data
 // print understandable data onto html
+
+
+var inputEl = $('#type');
+var buttonEl = $('.button');
+
+
+function clickVal() {
+    search = inputEl.val();
+	console.log(encodeURIComponent(search));
+	search.toString();
+	searchVal(search);
+		window.location.href = 'database.html';
+}
+
+function searchVal(search) {
+    
+
+	var settings = {
+		"async": true,
+		"crossDomain": true,
+		"url": `https://imdb8.p.rapidapi.com/title/find?q=${search}`,
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-key": "a9c67fd68amshcbae8866a9e3ac7p1f8029jsn2f3a9e8c0295",
+			"x-rapidapi-host": "imdb8.p.rapidapi.com"
+		}
+	};
+	
+	$.ajax(settings).done(function (response) {
+		console.log(response);
+	});
+ }
+
+
+ 
+
+
+buttonEl.on('click',searchVal);
+// inputEl.on('keypress', function (e) {
+//     if (e.key === 'Enter') {
+//       searchVal();
+//     }
+// });
+
+// put in input, add event listener click, get input value, build input value into query string, 
